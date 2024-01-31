0ab79ae18ad61906bdc37c60f1a52a768796c3f2
brianreicher <reicher.b@northeastern.edu>
2024-01-31 15:36:19 -0500

    build with actions

---
+++ scripts/fapi_script.py
@@ -0,0 +1,12 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+key: str = os.environ.get('ALPHAVANTAGE_KEY')
+url: str = f'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=IBM&apikey={key}'
+r: requests.Response = requests.get(url)
+data: dict = r.json()
+
+print(data)
